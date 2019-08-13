b9b99b487ad079b2d737f395b5cac518c53dc24d
quynhhgoogoo <luongdiemquynh1998@gmail.com>
2019-08-13 22:49:31 +0300

    basic info retrieved

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
-window.addEventListener("load", ()=>{
-  let long;
-  let lat;
-  let temperatureDescription = document.querySelector('.temperature-description');
-  let temperatureDegree = document.querySelector('.temperature-degree');
-  let locationTimezone = document.querySelector('.location-timezone');
-  
-  if(navigator.geolocation){
-  navigator.geolocation.getCurrentPosition(position=>{
-      console.log(position);
-      long = position.coords.longitude;
-      lat = position.coords.latitude;
-    
-      const proxy = "https://cors-anywhere.herokuapp.com/";
-      const api = `${proxy}https://api.darksky.net/forecast/6da00c8ed3dc597616f3e3675efa8cc3/${lat},${long}`;
-    
-      fetch(api)
-      .then(response=>{
-        return response.json();
-    })
-    .then(data=>{
-      console.log(data);
-      /*shorten the syntax*/
-      const{temperature, summary} = data.currently;
-        
-      /*Short DOM element from API*/
-      temperatureDegree.textContent = temperature;
-    });
-    });
-  }
+window.addEventListener("load", () => {
+    let long;
+    let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            console.log(position);
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+            const api = `${proxy}https://api.darksky.net/forecast/6da00c8ed3dc597616f3e3675efa8cc3/${lat},${long}`;
+
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    /*shorten the syntax*/
+                    const { temperature, summary, icon } = data.currently;
+
+                    /*Short DOM element from API*/
+                    temperatureDegree.textContent = temperature;
+                    temperatureDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone;
+
+                    //Set Icon
+                    setIcons(icon, );
+                });
+        });
+
+    }
+
+    function setIcons(icon, iconID) {
+        const skycons = new skycons({ color: "white" });
+        /*look for - and replace - with _ */
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
 });
